b7302d334bbc5a5a4ba0cfb2cb9bc757342a291b
Luan Nico <luanpotter27@gmail.com>
2016-03-19 11:35:53 -0300

    First commit

---
+++ src/test/java/xyz/luan/chatbot/utils/EndpointTestCase.java
@@ -0,0 +1,12 @@
+package xyz.luan.chatbot.utils;
+
+import io.yawp.testing.EndpointTestCaseBase;
+
+public class EndpointTestCase extends EndpointTestCaseBase {
+
+    @Override
+    protected String getAppPackage() {
+        return "xyz.luan.chatbot";
+    }
+
+}
